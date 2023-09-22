3855fdf3e14ca4ee9766893443dd1a01695eda00
Ryan BALOJI <rbaloji@asi.fr>
2023-09-22 17:42:30 +0200

    feat: add ssg end app

--- src/pages/ssg/[id].tsx
+++ src/pages/ssg/[id].tsx
@@ -1,13 +1,86 @@
-const SSGMovieDetailsPage = () => {
+import Like from '@/components/like/Like';
+import Note from '@/components/note/Note';
+import { Movie } from '@/interfaces/movie.interface';
+import { getMovieDetails, getMovies } from '@/services/movie.service';
+import {
+  GetStaticPaths,
+  GetStaticProps,
+  InferGetServerSidePropsType,
+} from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const SSGMovieDetailsPage = ({
+  movie,
+}: InferGetServerSidePropsType<typeof getStaticProps>) => {
+  const posterUrl = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+  const backdropPathUrl = `https://image.tmdb.org/t/p/original/${movie.backdrop_path}`;
+
   return (
     <main>
+      <Head>
+        <title>Static Site Generation</title>
+      </Head>
       <div className="flex md:flex-row flex-col">
-        <h1 className="flex justify-center w-full text-primary font-medium text-3xl pt-20">
-          Static Site Generation - Detail
-        </h1>
+        <div className="poster z-10 md:order-first order-last">
+          <Image
+            src={posterUrl}
+            alt={movie.title}
+            className="aspect-[2/3] object-cover h-full"
+            height={750}
+            width={500}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            priority
+          />
+        </div>
+        <div className="description relative w-full">
+          <Image
+            className="block z-0 object-cover brightness-50"
+            alt={movie.title}
+            src={backdropPathUrl}
+            fill
+            priority
+          />
+          <div className="relative flex flex-col">
+            <div className="flex flex-col gap-3 ml-4 text-white mt-3">
+              <h1 className="text-xl font-semibold">{movie.title}</h1>
+              <div className="flex gap-2">
+                {movie.genres.map((genre) => {
+                  return <p key={genre.id}>{genre.name}</p>;
+                })}
+              </div>
+              <p>{movie.tagline}</p>
+              <p className="mt-2 mr-10">{movie.overview}</p>
+              <div className="flex items-center gap-3">
+                <Note note={movie.vote_average} />
+                <Like id={movie.id} />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   );
+};
+
+export const getStaticProps: GetStaticProps<{
+  movie: Movie;
+}> = async ({ params }) => {
+  if (params?.id) {
+    const id = Number(params.id);
+    const movie = await getMovieDetails(id);
+    return { props: { movie } };
+  } else {
+    throw new Error('Missing id parameter');
+  }
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { results: movies } = await getMovies();
+  return {
+    paths: movies.map((movie) => `/ssg/${movie.id}`),
+    fallback: false,
+  };
 };
 
 export default SSGMovieDetailsPage;
--- src/pages/ssg/index.tsx
+++ src/pages/ssg/index.tsx
@@ -1,11 +1,39 @@
-const SSGPage = () => {
+import MovieCard from '@/components/movie/card/MovieCard';
+import { Movie } from '@/interfaces/movie.interface';
+import { getMovies } from '@/services/movie.service';
+import { GetStaticProps, InferGetServerSidePropsType } from 'next';
+import Head from 'next/head';
+
+const SSGPage = ({
+  movies,
+}: InferGetServerSidePropsType<typeof getStaticProps>) => {
+  const pathname = '/ssg';
   return (
-    <main className="lg:mx-44 mx-4 space-y-4 lg:pt-6 pt-14 pb-20">
-      <h1 className="text-center text-primary font-medium text-3xl pt-20">
-        Static Site Generation
-      </h1>
-    </main>
+    <>
+      <Head>
+        <title>Static Site Generation</title>
+      </Head>
+      <main className="lg:mx-44 mx-4 space-y-4 lg:pt-6 pt-14 pb-20">
+        <ul className="movies-list grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6">
+          {movies?.map((movie: Movie) => (
+            <li key={movie.id}>
+              <MovieCard movie={movie} pathname={pathname} />
+            </li>
+          ))}
+        </ul>
+        {movies.length < 1 && (
+          <p className="font-medium text-3xl">Aucun résultat</p>
+        )}
+      </main>
+    </>
   );
+};
+
+export const getStaticProps: GetStaticProps<{
+  movies: Movie[];
+}> = async () => {
+  const { results: movies } = await getMovies();
+  return { props: { movies } };
 };
 
 export default SSGPage;
