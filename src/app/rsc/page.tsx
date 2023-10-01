7ab868abb884ac7cab1935555f6c6a7691dd5d95
Olivier THIERRY <othierry@asi.fr>
2023-10-01 15:40:07 +0200

    Fix RSC default page

--- src/app/rsc/page.tsx
+++ src/app/rsc/page.tsx
@@ -2,7 +2,7 @@ const RSCPage = async () => {
   return (
     <main className="lg:mx-44 mx-4 space-y-4 lg:pt-6 pt-14 pb-20">
       <h1 className="text-center text-primary font-medium text-3xl pt-20">
-        React Server Component
+        React Server Components
       </h1>
     </main>
   );
