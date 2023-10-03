8a7088842bb24838b9d4fa74ec35a029fe13abd1
Ryan BALOJI <rbaloji@asi.fr>
2023-10-03 10:06:49 +0200

    fix: remove query params constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,13 +1,3 @@
 export const API_HEADER = {
   Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_KEY ?? 'empty'}`,
 };
-
-export const QUERY_PARAMS = {
-  PAGE: 'page',
-  LANGUAGE: 'language',
-  QUERY: 'query',
-};
-
-export const DEFAULT_PARAMS = {
-  LANGUAGE: 'fr-FR',
-};
