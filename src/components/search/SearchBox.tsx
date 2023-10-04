f711275149f0f39ce630f8906fb0d35ed2756735
Olivier THIERRY <othierry@asi.fr>
2023-10-04 10:39:13 +0200

    Remove SearchBox implementation

--- src/components/search/SearchBox.tsx
+++ src/components/search/SearchBox.tsx
@@ -1,27 +1,8 @@
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent } from 'react';
 
 const SearchBox = () => {
-  const router = useRouter();
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const [searchValue, setSearchValue] = useState<string | undefined>();
-
   const handleSearchChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const currentPathname = pathname ?? '/';
-    const value = event.target.value;
-    setSearchValue(value);
-
-    const queryParams = new URLSearchParams();
-    queryParams.append('query', encodeURI(value));
-
-    if (value.length > 3) {
-      router.push(`${currentPathname}?${queryParams.toString()}`);
-    }
-
-    if (value.length <= 2 && searchParams?.get('query')) {
-      router.push(currentPathname);
-    }
+    // TODO To implement
   };
 
   return (
@@ -29,7 +10,6 @@ const SearchBox = () => {
       type="text"
       className="search-input"
       placeholder="Recherche ..."
-      value={searchValue}
       onChange={handleSearchChange}
     />
   );
