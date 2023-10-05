c29b0e5750c80b99120e2e11af71e1007e577920
Ryan BALOJI <rbaloji@asi.fr>
2023-10-05 15:36:20 +0200

    feat: add experimental logging level

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    logging: {
+      level: 'verbose',
+    },
+  },
   images: {
     domains: ['image.tmdb.org'],
   },
