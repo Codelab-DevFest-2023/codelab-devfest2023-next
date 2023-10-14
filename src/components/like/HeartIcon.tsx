dcd0633ffe722e2cef1c10366b62201e0c9a38bb
Ryan BALOJI <rbaloji@asi.fr>
2023-10-14 14:07:30 +0200

    fix: refactor like component

---
+++ src/components/like/HeartIcon.tsx
@@ -0,0 +1,9 @@
+const HeartIcon = () => {
+  return (
+    <svg className="heart-icon" viewBox="0 0 26 26">
+      <path d="M12 21.35l-1.45-1.32C5.4 16.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C15.09 3.81 16.76 3 18.5 3 21.58 3 24 5.42 24 8.5c0 3.78-3.4 7.86-8.55 11.54L12 21.35z" />
+    </svg>
+  );
+};
+
+export default HeartIcon;
--- src/components/like/Like.tsx
+++ src/components/like/Like.tsx
@@ -1,59 +1,63 @@
 'use client';
 import { useEffect, useState } from 'react';
-import './like.css';
+import HeartIcon from './HeartIcon';
 
 interface Props {
   id: number;
 }
 
+interface Favorite {
+  id: number;
+}
+
 const Like = ({ id }: Props) => {
-  const [filled, setFilled] = useState(false);
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
 
   useEffect(() => {
     const favorites = localStorage.getItem('favorites');
     if (favorites) {
-      const isFind = JSON.parse(favorites).find(
-        (element: { id: number }) => element.id === id,
+      const isFound = JSON.parse(favorites).find(
+        (favorite: Favorite) => favorite.id === id,
       );
-      if (isFind) {
-        setFilled(true);
+      if (isFound) {
+        setIsFavorite(true);
       }
     } else {
-      setFilled(false);
+      setIsFavorite(false);
     }
   }, [id]);
 
   const handleClick = () => {
-    const favorites = localStorage.getItem('favorites');
-    if (favorites) {
-      const favoritesList = JSON.parse(favorites);
-
-      const filterFav = favoritesList.filter(
-        (fav: { id: number }) => fav.id !== id,
+    const storedFavorites = localStorage.getItem('favorites');
+    if (storedFavorites) {
+      const favoritesList: Favorite[] = JSON.parse(storedFavorites);
+      const hasFavorite = favoritesList.some(
+        (favorite: Favorite) => favorite.id === id,
       );
 
-      if (filterFav.length === favoritesList.length) {
-        setFilled(true);
-        favoritesList.push({ id: id });
+      if (!hasFavorite) {
+        favoritesList.push({ id });
         localStorage.setItem('favorites', JSON.stringify(favoritesList));
+        setIsFavorite(true);
       } else {
-        setFilled(false);
-        localStorage.setItem('favorites', JSON.stringify(filterFav));
+        const updatedFavoritesList = favoritesList.filter(
+          (favorite: Favorite) => favorite.id !== id,
+        );
+        localStorage.setItem('favorites', JSON.stringify(updatedFavoritesList));
+        setIsFavorite(false);
       }
     } else {
-      setFilled(true);
       localStorage.setItem('favorites', JSON.stringify([{ id: id }]));
+      setIsFavorite(true);
     }
   };
 
   return (
     <button
-      className={`heart-button ${filled ? 'filled' : ''}`}
+      className={`heart-button ${isFavorite ? 'filled' : ''}`}
       onClick={handleClick}
     >
-      <svg className="heart-icon" viewBox="0 0 26 26">
-        <path d="M12 21.35l-1.45-1.32C5.4 16.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C15.09 3.81 16.76 3 18.5 3 21.58 3 24 5.42 24 8.5c0 3.78-3.4 7.86-8.55 11.54L12 21.35z" />
-      </svg>
+      <HeartIcon />
     </button>
   );
 };
